1e758dd361a5d20827bfcc269ab5e887867d508c
Michael Duchemin <msducheminjr@users.noreply.github.com>
2024-12-11 20:33:57 -0500

    Iteration G3 Testing Our JavaScript Functionality
    Add system testing for the dynamic functionality from "Iteration G2:
    Adding Fields Dynamically to a Form."
    
    Modify the orders_test system test to add an item to the cart, click
    Checkout and then make assertions about the initial state of the new
    order page and then about the presence and absence of the dynamic fields
    when you select each pay type from the dropdown.
    
    Remove and clean up some of the system tests that were generated and
    kept passing but are no longer relevant.

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -5,16 +5,35 @@ class OrdersTest < ApplicationSystemTestCase
     @order = orders(:daves)
   end
 
-  test "visiting the index" do
-    visit orders_url
-    assert_selector "h1", text: "Orders"
-  end
+  test "check dynamic fields" do
+    visit store_index_url
 
-  test "trying to create an Order with no cart redirects to the store" do
-    visit new_order_url
+    click_on "Add to Cart", match: :first
 
-    assert_text "Your Pragmatic Catalog"
-    assert_text "Your cart is empty"
+    click_on "Checkout"
+    assert has_no_field? "Routing number"
+    assert has_no_field? "Account number"
+    assert has_no_field? "Credit card number"
+    assert has_no_field? "Expiration date"
+    assert has_no_field? "Po number"
+    select "Check", from: "Pay type"
+    assert has_field? "Routing number"
+    assert has_field? "Account number"
+    assert has_no_field? "Credit card number"
+    assert has_no_field? "Expiration date"
+    assert has_no_field? "Po number"
+    select "Credit card", from: "Pay type"
+    assert has_no_field? "Routing number"
+    assert has_no_field? "Account number"
+    assert has_field? "Credit card number"
+    assert has_field? "Expiration date"
+    assert has_no_field? "Po number"
+    select "Purchase order", from: "Pay type"
+    assert has_no_field? "Routing number"
+    assert has_no_field? "Account number"
+    assert has_no_field? "Credit card number"
+    assert has_no_field? "Expiration date"
+    assert has_field? "Po number"
   end
 
   test "should create order" do
@@ -27,30 +46,11 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
     select @order.pay_type, from: "Pay type"
-    click_on "Place Order"
+    assert_difference "Order.count" do
+      click_on "Place Order"
 
-    assert_text "Thank you for your order."
-    assert_text "Your Pragmatic Catalog"
-  end
-
-  test "should update Order" do
-    visit order_url(@order)
-    click_on "Edit this order", match: :first
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    select "Purchase order", from: "Pay type"
-    click_on "Place Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Order" do
-    visit order_url(@order)
-    click_on "Destroy this order", match: :first
-
-    assert_text "Order was successfully destroyed"
+      assert_text "Thank you for your order."
+      assert_text "Your Pragmatic Catalog"
+    end
   end
 end
