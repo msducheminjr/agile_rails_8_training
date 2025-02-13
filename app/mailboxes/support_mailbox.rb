098e3d24374d793525129ee3150feb9ecd8ca8b1
Michael Duchemin <msducheminjr@users.noreply.github.com>
2025-02-13 18:33:40 -0500

    Backport Task K Playtime improvements
    Bring some improvements from task K playtime back to the main branch:
    - Modify the SupportRequests::index action to eager load associations
      and rich texts for the responses. The dev server logs were causing
      multiple trips to the database for each rich text, order, line item,
      and product. An n+1 doesn't really matter when you have a handful of
      records, but fixed it anyway.
    - Refactor support_mailbox to assign mail.from_address.to_s to a
      variable instead of repeating it
    - Add support requests to the application layout sidebar

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,10 +1,11 @@
 class SupportMailbox < ApplicationMailbox
   def process
-    recent_order = Order.where(email: mail.from_address.to_s).
+    email = mail.from_address.to_s
+    recent_order = Order.where(email: email).
                         order("created_at desc").
                         first
     SupportRequest.create!(
-      email: mail.from_address.to_s,
+      email: email,
       subject: mail.subject,
       body: mail.body.to_s,
       order: recent_order
